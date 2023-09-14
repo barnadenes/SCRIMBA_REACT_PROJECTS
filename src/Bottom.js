addfda384d834a70a66369ada15b23a9b753d426
Barna Denes <nyukk1007@gmail.com>
2023-09-14 16:06:41 +0200

    good enough

--- src/Bottom.js
+++ src/Bottom.js
@@ -1,14 +1,14 @@
 function Card() {
     return (
         <div className="card">
-            <img src="../airbnb-2-logo-png-transparent.png" width="100" className="card--image" />
+            <img src="../Minx.jpg" width="100" height="120" className="card--image" />
             <div className="card--stats">
                 <img src="../favicon.ico" width="10" className="card--star" />
                 <span>5.0</span>
                 <span>(6) • </span>
                 <span>USA</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
+            <p>Life Lessons with JustaMinx</p>
             <p>From $136 / person</p>
         </div>
     )
